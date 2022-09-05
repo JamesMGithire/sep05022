eb82627ba49dd021326fe2f9610a768933d72f26
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-05 10:27:23 +0300

    Spaced image and title

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <div className="br9 bg-red pad9 display-flex jc-c">
-            <span>Travel Journal</span>
+            <span className="prl5">Travel Journal</span>
             <img src="./logo192.png" className="App-logo" alt="react-logo"/>
         </div>
     )
