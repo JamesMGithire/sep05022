f1cc444d4441afa88639513a0ec05fe68c3f9196
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-05 09:52:12 +0300

    Completed project

--- src/Location.js
+++ src/Location.js
@@ -1,12 +1,12 @@
 export default function Location(props){
     return (
-        <div className="ta-l pad9 display-flex">
-            <img src={props.image} alt="" className="d-inline bra9"/>
+        <div className="ta-l pad9 display-flex lh10">
+            <div className="d-inline bra9 height100px bg-red" style={{backgroundImage: `url(${props.image})`}}></div>
             <div className="pad9">
-                <p>{props.country}<a></a></p>
+                <p className="pt12">{props.country}  <a href={props.googleMaps}><small>View on Google Maps</small></a></p>
                 <h2>{props.location}</h2>
-                <p><small><b></b></small></p>
-                <p></p>
+                <p className="pt12"><small><b>{props.date}</b></small></p>
+                <p className="fs12 pt12">{props.content}</p>
             </div>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -2,25 +2,28 @@ export default [
     {
         id: 1,
         country: "Japan",
+        googleMaps:"https://goo.gl/maps/aQRocJv8J2bJgE4y9",
         location: "Mount Fuji",
         date: "Some time",
         content: "I liked this place",
-        image:"./buddha-location.jpg"
+        image:"./fuji-mountains-green-tea-plantation.jpg"
     },
     {
         id: 2,
         country: "Austria",
+        googleMaps:"https://goo.gl/maps/5j8weqidvLFbLvh66",
         location: "Sydney Opera House",
         date: "Some time",
         content: "I liked this place",
-        image:"./buddha-location.jpg"
+        image:"./sydney-opera-house.jpg"
     },
     {
         id: 3,
         country: "Norway",
+        googleMaps:"https://goo.gl/maps/2gTq8ug3RqGfJ1ir6",
         location: "Geirangerfjord",
         date: "Some time",
         content: "I liked this place",
-        image:"./buddha-location.jpg"
+        image:"./beautiful-nature-norway-natural.jpg"
     },
 ]
